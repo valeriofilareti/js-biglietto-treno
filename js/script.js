e9c05f4b92ede54f552ec62f569bb7a35b471f97
valeriofilareti <valerio.filareti@gmail.com>
2023-03-20 23:32:53 +0100

    [edit] script

--- js/script.js
+++ js/script.js
@@ -3,14 +3,10 @@ const km = prompt('quanti km vuoi percorrere?')
 
 let price = km * 0.21;
 
-console.log(price);
-
 if(age < 18) {
-  price = price - (price / 100 * 20);
-} else if (age > 65) {
-  price = price - (price / 100 * 40);
-} else {
-  price = price;
-}
-console.log(price.toFixed(2));
+  price -= (price / 100 * 20);
+}else if(age > 65) {
+  price -= (price / 100 * 40);
+} 
+
 document.getElementById('price').innerHTML = price.toFixed(2) + '€'
